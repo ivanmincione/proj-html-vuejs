e66086872bddc06221a35d7763713a8784cde1b3
Ivan Mincione <ivan.mincione92@outlook.it>
2020-12-01 20:18:11 +0100

    Start fix code

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ var app = new Vue ({
     el:'#root',
     data: {
         items: [ "Home","About", "Services", "Journal", "Contact"],
+        show: false,
     } ,
 
+    methods: {
 
+        autoClose() {
+            setTimeout(() => {
+                this.show = false;
+            }, 3000);
+        }
+    // end methods
+    }
 //end Vue
 });
