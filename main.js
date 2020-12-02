d0e45a5fd54e7ce92ec25d22ddc165becb3c18a7
Ivan Mincione <ivan.mincione92@outlook.it>
2020-12-02 11:25:56 +0100

    Fix data in Vue
    Checked structure in data

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 var app = new Vue ({
     el:'#root',
     data: {
-        items: [ "Home","About", "Services", "Journal", "Contact"],
+        items: [
+            {
+                name: "Home",
+            },
+            {
+                name: "About",
+            },
+            {
+                name: "Services",
+                links: [],
+            },
+            {
+                name: "Journal",
+            },
+            {
+                name: "Contact",
+            },
+
+        ],
+        //oggetto per i mex della chat
+        chat:{
+            mex:"This is a free Hubspot live chat. You can create your own chat flows and engage your customers with Avada& Hubspot!",
+        },
         show: false,
     } ,
 
@@ -11,8 +33,9 @@ var app = new Vue ({
             setTimeout(() => {
                 this.show = false;
             }, 3000);
-        }
-    // end methods
+        },
+
+
     }
 //end Vue
 });
